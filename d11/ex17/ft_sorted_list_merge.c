d23efab2b6998cef8b5b404cfe6fab8335aa5a50
rems14 <rems14@lestoutous.com>
2017-08-07 00:52:33 +0200

    d11/ex17 fix bug list empty

--- d11/ex17/ft_sorted_list_merge.c
+++ d11/ex17/ft_sorted_list_merge.c
@@ -13,17 +13,18 @@
 #include "ft_list.h"
 
 void	ft_sorted_aux(t_list **begin_list1, t_list *begin_list2
-		, t_list *cur1, t_list **prev1)
+		, t_list **cur1, t_list **prev1)
 {
-	if (cur1 == *begin_list1)
+	if (*cur1 == *begin_list1)
 	{
 		*begin_list1 = begin_list2;
-		(*begin_list1)->next = cur1;
+		(*begin_list1)->next = *cur1;
+		*cur1 = *begin_list1;
 	}
 	else
 	{
 		(*prev1)->next = begin_list2;
-		(*prev1)->next->next = cur1;
+		(*prev1)->next->next = *cur1;
 		*prev1 = (*prev1)->next;
 	}
 }
@@ -35,7 +36,7 @@ void	ft_sorted_list_merge(t_list **begin_list1
 	t_list	*cur1;
 	t_list	*next2;
 
-	if (*begin_list1 && begin_list2)
+	if (begin_list2)
 	{
 		prev1 = 0;
 		cur1 = *begin_list1;
@@ -44,10 +45,10 @@ void	ft_sorted_list_merge(t_list **begin_list1
 		{
 			if (!cur1 || (*cmp)(begin_list2->data, cur1->data) <= 0)
 			{
-				ft_sorted_aux(begin_list1, begin_list2, cur1, &prev1);
+				ft_sorted_aux(begin_list1, begin_list2, &cur1, &prev1);
 				begin_list2 = next2;
 				if (next2)
-					next2 = next2->next ? next2->next : 0;
+					next2 = next2->next;
 			}
 			else
 			{
