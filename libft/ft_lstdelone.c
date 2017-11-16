b72b0392665c605ae2e7b6e5b8c49ac3f85be808
rems14 <rems14@lestoutous.com>
2017-11-16 11:08:14 +0100

    fix ft_lstdelone

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 15:54:52 by rbarbero          #+#    #+#             */
-/*   Updated: 2017/11/15 16:03:18 by rbarbero         ###   ########.fr       */
+/*   Updated: 2017/11/16 11:05:16 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
 	del((*alst)->content, (*alst)->content_size);
 	free(*alst);
-	alst = NULL;
+	*alst = NULL;
 }
