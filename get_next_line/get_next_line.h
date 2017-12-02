d32277c9ba852b343116c878da9abbaa67c3da7a
Remi BARBERO <rbarbero@student.42.fr>
2017-12-02 12:45:39 +0100

    git subrepo clone vogsphere@vogsphere.42.fr:intra/2017/activities/get_next_line/rbarbero get_next_line
    subrepo:
      subdir:   "get_next_line"
      merged:   "c9eea05"
    upstream:
      origin:   "vogsphere@vogsphere.42.fr:intra/2017/activities/get_next_line/rbarbero"
      branch:   "master"
      commit:   "c9eea05"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/26 09:23:35 by rbarbero          #+#    #+#             */
+/*   Updated: 2017/12/01 23:18:39 by rbarbero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 4096
+
+# include "libft.h"
+
+typedef struct	s_buf
+{
+	int				fd;
+	t_list			*lchar;
+	unsigned int	count_nl;
+}				t_buf;
+
+int				get_next_line(const int fd, char **line);
+
+#endif
